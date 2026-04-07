b39e060cc664eaba3d7bf6a741a264a436835462
Kiran Linge <kiranlinge9@gmail.com>
2026-04-07 19:41:41 +0530

    Initial commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a=56;
+        int b=64;
+        int sum =a+b;
+        System.out.println(sum);
+
+    }
+}
---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int a=13;
+        int b=5;
+        System.out.println(a);
+        System.out.println(b);
+        String name="Tony Stark";
+        System.out.println(name);
+        a=50;
+        System.out.println(a);
+    }
+}
