99352910e8c5b0191abebb9311bf86bac8b28340
Kiran Linge <kiranlinge9@gmail.com>
2026-04-08 08:55:47 +0530

    Cleaned project and removed compiled files

---
+++ homework.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class homework {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    float a=sc.nextFloat();
+    float b=sc.nextFloat();
+    float c=sc.nextFloat();
+    float sum = (a+b+c)/3;
+    System.out.println(sum);
+     sc.close();
+        }
+}
